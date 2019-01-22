780368a97fe299991d52933586a86493dac7c66f
Satoki Tsuichihara <satoki-t@rs.tus.ac.jp>
2019-01-22 19:19:03 +0900

    add research contents and include function

---
+++ js/header.js
@@ -0,0 +1,9 @@
+document.write('\
+  <div>\
+    <ul>\
+      <li><a href="index.html">PROFILE</a></li>\
+      <li><a href="research.html">RESEARCH</a></li>\
+      <li><a href="publication.html">PUBLICATION</a></li>\
+      <li><a href="contact.html">CONTACT</a></li>\
+    </ul></div>');
+// document.write('<div class="header">ヘッダーです</div>');
